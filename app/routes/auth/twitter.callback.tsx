fe0df59d74b1ae728216d68e44cd4b9cafa16d85
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-08-22 09:11:10 -0500

    authenticate twitter from callback

--- app/routes/auth/twitter.callback.tsx
+++ app/routes/auth/twitter.callback.tsx
@@ -1,6 +1,8 @@
 import type { LoaderArgs } from "@remix-run/node"
 import { redirect } from "@remix-run/node"
+import { authenticator } from "~/auth"
 
 export async function loader({ request }: LoaderArgs) {
+  await authenticator.authenticate("twitter", request)
   return redirect("/")
 }
