60a2362d3e8648e18f53503455fd5d6b57b051c2
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-08-22 21:00:18 -0500

    separate login route

--- app/routes/auth/logout.tsx
+++ app/routes/auth/logout.tsx
@@ -2,5 +2,5 @@ import type { ActionArgs } from "@remix-run/server-runtime"
 import { authenticator } from "~/auth.server"
 
 export function action({ request }: ActionArgs) {
-  return authenticator.logout(request, { redirectTo: "/" })
+  return authenticator.logout(request, { redirectTo: "/login" })
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,18 +1,21 @@
-import { Form, useLoaderData, useTransition } from "@remix-run/react"
-import type { LoaderArgs } from "@remix-run/server-runtime"
-import { json } from "@remix-run/server-runtime"
-import { FiHeart, FiLogOut, FiTwitter } from "react-icons/fi"
+import type { LoaderArgs } from "@remix-run/node"
+import { json, redirect } from "@remix-run/node"
+import { Form, useLoaderData } from "@remix-run/react"
+import { FiLogOut } from "react-icons/fi"
 import { authenticator } from "~/auth.server"
 import { outlineButtonClass } from "~/styles"
 
 export async function loader({ request }: LoaderArgs) {
   const user = await authenticator.isAuthenticated(request)
+  if (!user) {
+    return redirect("/login")
+  }
   return json({ user })
 }
 
 export default function Index() {
   const { user } = useLoaderData<typeof loader>()
-  return user ? (
+  return (
     <main className="flex flex-col items-center gap-4">
       <p>hi, {user.name}!</p>
       <Form method="post" action="/auth/logout" className="contents">
@@ -21,41 +24,6 @@ export default function Index() {
           sign out
         </button>
       </Form>
-    </main>
-  ) : (
-    <Welcome />
-  )
-}
-
-function Welcome() {
-  const transition = useTransition()
-  return (
-    <main className="flex flex-col items-center max-w-sm text-center gap-8">
-      <h1 className="font-light text-4xl leading-none">
-        hi, welcome to moodii!
-      </h1>
-      <p>
-        we don't always feel our best. moodii is a simple app to help you track
-        how you're feeling over time. <FiHeart className="inline-block" />
-      </p>
-      <div className="flex flex-col items-center gap-4 w-full max-w-xs">
-        <Form method="post" action="/auth/twitter/login" className="contents">
-          <button type="submit" className={outlineButtonClass}>
-            <FiTwitter />
-            sign in with twitter
-          </button>
-          {transition.submission?.action === "/auth/twitter/login" && (
-            <p>
-              <span className="inline-block mr-2">just a moment...</span>
-              <span className="inline-grid grid-cols-2 w-4 h-4 gap-0.5 animate-spin align-middle">
-                {Array.from({ length: 4 }, (_, i) => (
-                  <span key={i} className="rounded-full bg-white" />
-                ))}
-              </span>
-            </p>
-          )}
-        </Form>
-      </div>
     </main>
   )
 }
